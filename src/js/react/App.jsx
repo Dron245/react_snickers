a86fa9c8b0347c4dc56584fe5fab7be2f2502195
Dron245 <Dron245@yandex.ru>
2024-05-28 21:06:58 +0300

    2805-2106

---
+++ src/js/react/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<>
+		<h3 className="qwe">Привет</h3>
+		</>
+	)
+	
+};
+
+export default App
--- src/js/react/Index.jsx
+++ src/js/react/Index.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx"
 
-import someImg from "@img/cover.jpg";
-
-const App = () => (
-	<>
-		<h1 className="title">В комнату заходит React</h1>
-		<img
-			src={someImg}
-			alt="Image"
-		/>
-	</>
-);
 
 // Объект для вывода
 const root = document.querySelector("#root")
