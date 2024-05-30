76e369bbed5a698e17b993ea72a59c396f9b8618
Dron245 <Dron245@yandex.ru>
2024-05-30 16:19:13 +0300

    3005-1619

--- src/js/react/App.jsx
+++ src/js/react/App.jsx
@@ -1,59 +1,37 @@
 import React from "react";
-
+import Header from "./components/Header.jsx";
+import Card from "./components/Card/Card.jsx";
+import Driwer from "./components/Driwer.jsx";
+import  {arr}  from "./data.js";
 const App = () => {
+	
+	const deistvie = () => {
+		console.log(123);
+	}
 	return (
 		<>
-			<header className="header">
-				<div className="header__container">
-					<div className="header__left">
-						<div className="header__img">
-							<img src="img/logo.svg" alt="logo"/>
-						</div>
-						<div className="header__title-block">
-							<h2 className="header__title">REACT SNEAKERS</h2>
-							<div className="header__text">Магазин лучших кроссовок</div>
-						</div>
-					</div>
-					<div className="header__actions actions-header">
-						<div className="actions-header__item">
-							<div className="actions-header__img">
-								<img src="@img/cart.svg" alt="cart"/>
-							</div>
-							<span className="actions-header__text"><strong>1205 руб.</strong></span>
-						</div>
-						<div className="actions-header__item">
-							<div className="actions-header__img">
-								<img src="@img/favorite.svg" alt="favorite"/>
-							</div>
-							<span className="actions-header__text">Закладки</span>
-						</div>
-						<div className="actions-header__item">
-							<div className="actions-header__img">
-								<img src="@img/user.svg" alt="user"/>
-							</div>
-							<span className="actions-header__text">Профиль</span>
-						</div>
-					</div>
-				</div>
-			</header>
+			<Driwer/>
+			<Header/>
 			<main>
 				<section className="page">
 					<div className="page__container">
-						<h1 className="page__title">Все кроссовки</h1>
+						<div className="page__top">
+							<h1 className="page__title">Все кроссовки</h1>
+							<div className="page__search">
+								<img src="@img/search-input.svg" alt=""/>
+								<input id="input" className="page__input" placeholder="Поиск" type="text" />
+							</div>
+						</div>
 						<div className="page__body">
-							<article className="page__item card">
-								<img src="@img/01.jpg" alt=""/>
-								<h3 className="card__text">Мужские Кроссовки Nike Blazer Mid Suede</h3>
-								<div className="card__block">
-									<div className="card__left">
-										<span className="card__cost">цена</span>
-										<span className="card__cost-number">12 999 руб.</span>
-									</div>
-									<button className="card__button">
-										<img src="@img/add.svg" alt=""/>
-									</button>
-								</div>
-							</article>
+							{arr.map((obj) =>
+							<Card
+								key={obj.title}
+								title={obj.title}
+								cost={obj.cost}
+								imageUrl={obj.imageUrl}
+								onClick={deistvie}
+							/>
+							)}
 						</div>
 					</div>
 				</section>
